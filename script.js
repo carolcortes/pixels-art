7e9954ede240841925ec27949537fd5a92cd79e3
carolcortes <caroline.ocortes@gmail.com>
2022-04-20 18:35:42 -0300

    requisito 4

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 console.log('Olá, mundo!');
-const colorPallet = document.getElementById('color-palette');
-const rgbColors = ['rgb(69, 41, 102)', 'rgb(152, 32, 101)', 'rgb(217, 91, 91)',
-  'rgb(255, 180, 110)', 'rgb(229, 230, 151)', 'rgb(81, 133, 104)'];
 
-for (let count = 0; count < 6; count += 1) {
+// create colorDivs
+const colorPallet = document.getElementById('color-palette');
+for (let count = 0; count < 4; count += 1) {
   const colorDiv = document.createElement('div');
   colorDiv.className = 'color';
   colorPallet.appendChild(colorDiv);
 }
 
+// set color
 const colorDiv = document.getElementsByClassName('color');
+const rgbColors = ['rgb(69, 41, 102)', 'rgb(152, 32, 101)', 'rgb(217, 91, 91)',
+  'rgb(255, 180, 110)'];
+const firstColor = document.getElementsByClassName('color')[0];
 for (let index = 0; index < colorDiv.length; index += 1) {
   colorDiv[index].style.backgroundColor = rgbColors[index];
 }
+firstColor.style.backgroundColor = 'black';
 
+// create coloring board
+const pixelBoard = document.getElementById('pixel-board');
+for (let line = 0; line < 5; line += 1) {
+  const pixelLine = document.createElement('div');
+  pixelLine.className = 'lines';
+  pixelBoard.appendChild(pixelLine);
+}
+const line = document.getElementsByClassName('lines');
+for (let index = 0; index < line.length; index += 1) {
+  for (let width = 0; width < 5; width += 1) {
+    const pixelDiv = document.createElement('div');
+    pixelDiv.className = 'pixel';
+    line[index].appendChild(pixelDiv);
+  }
+}
