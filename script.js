a3f08d7c67152a6add040b33db294ae5c18e8a33
carolcortes <caroline.ocortes@gmail.com>
2022-04-20 16:59:39 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Olá, mundo!')
+console.log('Olá, mundo!');
+const colorPallet = document.getElementById('color-palette');
+const rgbColors = ['rgb(69, 41, 102)', 'rgb(152, 32, 101)', 'rgb(217, 91, 91)',
+  'rgb(255, 180, 110)', 'rgb(229, 230, 151)', 'rgb(81, 133, 104)'];
+
+for (let count = 0; count < 6; count += 1) {
+  const colorDiv = document.createElement('div');
+  colorDiv.className = 'color';
+  colorPallet.appendChild(colorDiv);
+}
+
+const colorDiv = document.getElementsByClassName('color');
+for (let index = 0; index < colorDiv.length; index += 1) {
+  colorDiv[index].style.backgroundColor = rgbColors[index];
+}
+
